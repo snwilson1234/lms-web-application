f41566373409c7306bf1e3de813d1d4131f781d5
Shelby Wilson <97818875+snw0502@users.noreply.github.com>
2023-09-27 20:51:46 -0400

    initial commit from progress before creating repo

---
+++ script.js
@@ -0,0 +1,24 @@
+function toggleUserMenu(){
+    var popup = document.getElementById('user');
+    popup.classList.toggle('active');
+}
+
+function toggleCoursesMenu(){
+    var popup = document.getElementById('courses');
+    popup.classList.toggle('active');
+}
+
+function toggleGroupsMenu(){
+    var popup = document.getElementById('groups');
+    popup.classList.toggle('active');
+}
+
+function toggleHistoryMenu(){
+    var popup = document.getElementById('history');
+    popup.classList.toggle('active');
+}
+
+function toggleHelpMenu(){
+    var popup = document.getElementById('help');
+    popup.classList.toggle('active');
+}
