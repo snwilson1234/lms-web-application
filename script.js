6932da41aa4b93c1924252c427381927e37f9d83
Shelby Wilson <97818875+snw0502@users.noreply.github.com>
2023-09-28 22:41:45 -0400

    added dynamic class setion re-rendering for Home, Announcements buttons in class menu bar

--- script.js
+++ script.js
@@ -1,3 +1,53 @@
+
+
+// class ClassMiddleHandler {
+//     constructor(content){
+//         this.content = content;
+//     }
+
+//     setContent(content) {
+//         this.content = content;
+//     }
+// }
+
+// let fileName = location.href.split("/").slice(-1);
+
+// console.debug(fileName);
+
+// let
+
+// if (fileName == 'sample-class-page.html'){
+    
+// }
+
+function toggleClassContent(buttonId) {
+    const homeContent = document.getElementById('home-content');
+    const announcementContent = document.getElementById('announcement-content');
+    //const syllabusContent = document.getElementById('syllabus-content');
+    //const modulesContent = document.getElementById('modules-content');
+    //const assignmentsContent = document.getElementById('assignments-content');
+
+    homeContent.classList.remove('active');
+    announcementContent.classList.remove('active');
+    //syllabusContent.classList.remove('active');
+    //modulesContent.classList.remove('active');
+    //assignmentsContent.classList.remove('active');
+    
+
+    if (buttonId === 'btn-home') {
+        homeContent.classList.toggle('active');
+    } else if (buttonId === 'btn-announcement') {
+        announcementContent.classList.toggle('active');
+    } else if (buttonId === 'btn-syllabus') {
+        syllabusContent.classList.toggle('active');
+    } else if (buttonId === 'btn-modules') {
+        modulesContent.classList.toggle('active');
+    } else if (buttonId === 'btn-assignments') {
+        assignmentsContent.classList.toggle('active');
+    }
+}
+
+
 function toggleUserMenu(){
     var popup = document.getElementById('user');
     popup.classList.toggle('active');
