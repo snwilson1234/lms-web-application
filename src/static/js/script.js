99c2911b58b4078726ca9356959001065682310f
Shelby Wilson <97818875+snw0502@users.noreply.github.com>
2023-10-26 10:05:03 -0700

    upload tab rough code

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -50,3 +50,26 @@ function togglePopup(menu_name) {
         helpMenu.classList.add('active');
     }
 }
+
+
+//Assignment upload tab functionality
+function openCity(evt, elem_name) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("upload-tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(elem_name).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
